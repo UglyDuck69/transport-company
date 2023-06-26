080c3b98042103bcf822609281e0380f15cc0619
გიორგი მეტრეველი <metreveli251@gmail.com>
2023-06-26 20:59:35 +0400

    master

--- javascript.js
+++ javascript.js
@@ -4,3 +4,18 @@ const menuLinks = document.getElementById("menu-links");
 menuBtn.addEventListener("click", () => {
   menuLinks.classList.toggle("show");
 });
+
+
+
+var myDiv = document.getElementById('arrow-left');
+function gadartva() {
+  window.alert("Div Clicked");
+};
+
+var image = document.getElementById('myImage');
+    
+    // Add a click event listener to the image
+    image.addEventListener('click', function() {
+      // Change the window location to the desired URL
+      window.location.href = 'https://www.facebook.com';
+    });
